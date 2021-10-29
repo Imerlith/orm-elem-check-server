ccb1d6e9b00749386b50e5a91ec632eb04bd1b99
Przemysław Gołębski <przemyslawgolebski@gmail.com>
2021-10-29 21:09:19 +0200

    Extra tests for orm-elem.go

--- orm/orm-elem.go
+++ orm/orm-elem.go
@@ -1,14 +1,38 @@
 package orm
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 func CheckCombinations(elements [][]string) error {
 	if elements == nil {
 		return errors.New("elements cannot be nil")
 	}
-	if len(elements) < 1 {
-		return errors.New("elements cannot be empty")
+	if len(elements) < 2 {
+		return errors.New("elements cannot have less than 2 rows")
 	}
-
+	if len(elements[0]) < 2 {
+		return errors.New("elements cannot have less than 2 columns")
+	}
+	if !areDimCorrect(elements) {
+		return errors.New("elements do not have constant dimension size")
+	}
 	return nil
+}
+
+func areDimCorrect(elements [][]string) bool {
+	maxSize := len(elements[0])
+	for i := 1; i < len(elements); i++ {
+		rowSize := 0
+		for _, element := range elements[i] {
+			if strings.TrimSpace(element) != "" {
+				rowSize++
+			}
+		}
+		if maxSize != rowSize {
+			return false
+		}
+	}
+	return true
 }
--- orm/orm-elem_test.go
+++ orm/orm-elem_test.go
@@ -11,7 +11,109 @@ func Test_CheckCombinations_Empty_Elems(t *testing.T) {
 		t.Fatal("Should pass an error")
 	}
 
-	if err.Error() != "elements cannot be empty" {
+	if err.Error() != "elements cannot have less than 2 rows" {
+		t.Fatal("Wrong error message")
+	}
+}
+
+func Test_CheckCombinations_Null_Elems(t *testing.T) {
+	err := CheckCombinations(nil)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+
+	if err.Error() != "elements cannot be nil" {
+		t.Fatal("Wrong error message")
+	}
+}
+
+func Test_CheckCombinations_OneRow_Elems(t *testing.T) {
+	var elems = [][]string{
+		{
+			"a1", "b1", "c1",
+		},
+	}
+
+	err := CheckCombinations(elems)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+
+	if err.Error() != "elements cannot have less than 2 rows" {
+		t.Fatal("Wrong error message")
+	}
+}
+
+func Test_CheckCombinations_OneColumn_Elems(t *testing.T) {
+	var elems = [][]string{
+		{
+			"a1",
+		},
+		{
+			"a2",
+		},
+		{
+			"a3",
+		},
+	}
+
+	err := CheckCombinations(elems)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+
+	if err.Error() != "elements cannot have less than 2 columns" {
+		t.Fatal("Wrong error message")
+	}
+}
+
+func Test_CheckCombinations_WrongDimensions_TooShort(t *testing.T) {
+	var elems = [][]string{
+		{
+			"a1", "b1", "c1",
+		},
+		{
+			"a2", "b2", "",
+		},
+		{
+			"a3", "b3", "c3",
+		},
+	}
+
+	err := CheckCombinations(elems)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+
+	if err.Error() != "elements do not have constant dimension size" {
+		t.Fatal("Wrong error message")
+	}
+}
+
+func Test_CheckCombinations_WrongDimensions_TooLong(t *testing.T) {
+	var elems = [][]string{
+		{
+			"a1", "b1", "c1",
+		},
+		{
+			"a2", "b2", "c1",
+		},
+		{
+			"a3", "b3", "c3", "d4",
+		},
+	}
+
+	err := CheckCombinations(elems)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+
+	if err.Error() != "elements do not have constant dimension size" {
 		t.Fatal("Wrong error message")
 	}
 }
