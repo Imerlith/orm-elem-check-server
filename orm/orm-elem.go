bca0ccd293b61f7459cebd271c8e7dabddbcac8f
Przemyslaw Golebski <przemyslawgolebski@gmail.com>
2021-11-06 00:40:41 +0100

    Added getColumn function This returns culumn for position x from two dimensional array

--- orm/orm-elem.go
+++ orm/orm-elem.go
@@ -15,13 +15,19 @@ func CheckCombinations(elements [][]string) error {
 	if len(elements[0]) < 2 {
 		return errors.New("elements cannot have less than 2 columns")
 	}
-	if !areDimCorrect(elements) {
+	if !areDimsCorrect(elements) {
 		return errors.New("elements do not have constant dimension size")
 	}
+
+	//TODO: Add spliting
+	var splitedElements = [][]string{}
+	_ = removeDuplicates(splitedElements)
+
+
 	return nil
 }
 
-func areDimCorrect(elements [][]string) bool {
+func areDimsCorrect(elements [][]string) bool {
 	maxSize := len(elements[0])
 	for i := 1; i < len(elements); i++ {
 		rowSize := 0
@@ -35,4 +41,44 @@ func areDimCorrect(elements [][]string) bool {
 		}
 	}
 	return true
+}
+
+func removeDuplicates(elements [][]string) [][]string {
+	var elemsWithoutDuplicates = [][]string{}
+	var rowsIds = []string{}
+	for _, row := range elements {
+		rowId := strings.Join(row, "")
+		if isElementPresent(rowId, rowsIds) {
+			continue
+		}
+		rowsIds = append(rowsIds, rowId)
+		elemsWithoutDuplicates = append(elemsWithoutDuplicates, row)
+	}
+	return elemsWithoutDuplicates
+}
+
+func isElementPresent(element string, arr []string) bool {
+	for _, ae := range arr {
+		if element == ae {
+			return true
+		}
+	}
+	return false
+}
+
+func getColumn(elements [][]string, columnNumber int) ([]string, error) {
+	if elements == nil {
+		return nil, errors.New("elements cannot be nil")
+	}
+	if len(elements) < 1 {
+		return nil, errors.New("elements must contain at least one row")
+	}
+	if columnNumber < 0 || columnNumber >= len(elements[0]) {
+		return nil, errors.New("columnNumber must be in <0, len)")
+	}
+	column := []string{}
+	for _, row := range elements {
+		column = append(column, row[columnNumber])
+	}
+	return column, nil
 }
--- orm/orm-elem_test.go
+++ orm/orm-elem_test.go
@@ -117,3 +117,129 @@ func Test_CheckCombinations_WrongDimensions_TooLong(t *testing.T) {
 		t.Fatal("Wrong error message")
 	}
 }
+
+
+func Test_getColumn_nilElements(t* testing.T) {
+	column, err := getColumn(nil, 1)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+	if err.Error() != "elements cannot be nil" {
+		t.Fatal("Wrong error message")
+	}
+	if column != nil {
+		t.Fatal("Column should be nil")
+	}
+}
+
+func Test_getColumn_emptyElements(t* testing.T) {
+	elements := [][]string{}
+
+	column, err := getColumn(elements, 1)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+	if err.Error() != "elements must contain at least one row" {
+		t.Fatal("Wrong error message")
+	}
+	if column != nil {
+		t.Fatal("Column should be nil")
+	}
+}
+
+func Test_getColumn_columnNumberEqualLen(t* testing.T) {
+	elements := [][]string{
+		{
+			"a1", "b1", "c1",
+		},
+	}
+
+	column, err := getColumn(elements, 3)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+	if err.Error() != "columnNumber must be in <0, len)" {
+		t.Fatal("Wrong error message")
+	}
+	if column != nil {
+		t.Fatal("Column should be nil")
+	}
+}
+
+func Test_getColumn_columnNumberGreaterThanLen(t* testing.T) {
+	elements := [][]string{
+		{
+			"a1", "b1", "c1",
+		},
+	}
+
+	column, err := getColumn(elements, 5)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+	if err.Error() != "columnNumber must be in <0, len)" {
+		t.Fatal("Wrong error message")
+	}
+	if column != nil {
+		t.Fatal("Column should be nil")
+	}
+}
+
+func Test_getColumn_columnNumberLessThanZero(t* testing.T) {
+	elements := [][]string{
+		{
+			"a1", "b1", "c1",
+		},
+	}
+
+	column, err := getColumn(elements, -1)
+
+	if err == nil {
+		t.Fatal("Should pass an error")
+	}
+	if err.Error() != "columnNumber must be in <0, len)" {
+		t.Fatal("Wrong error message")
+	}
+	if column != nil {
+		t.Fatal("Column should be nil")
+	}
+}
+
+func Test_getColumn_correctElemsAndNumber(t* testing.T) {
+	elements := [][]string{
+		{
+			"a1", "b1", "c1",
+		},
+		{
+			"a2", "b2", "c2",
+		},
+		{
+			"a3", "b3", "c3",
+		},
+	}
+
+	column, err := getColumn(elements, 1)
+	expectedColumn := []string{
+		"b1", "b2", "b3",
+	}
+
+	if err != nil {
+		t.Fatal("Should not pass an error")
+	}
+	if column == nil {
+		t.Fatal("Column should not be nil")
+	}
+	if len(expectedColumn) != len(column)  {
+		t.Fatalf("Wrong column size received. Should be: %v but is %v", len(expectedColumn), len(column))
+	}
+
+	for index, item := range column {
+		if item != expectedColumn[index] {
+			t.Fatalf("Column element missmatch. Should be: %v but is %v", expectedColumn[index], item)
+		}
+	}
+}
